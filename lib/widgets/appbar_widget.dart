b8f4976011ffdb0bdca44e82f036690f256bd0fa
Adriell8000B <adrielluiz73@gmail.com>
2026-02-09 11:24:00 -0300

    Added pop menu button

--- lib/widgets/appbar_widget.dart
+++ lib/widgets/appbar_widget.dart
@@ -8,6 +8,33 @@ class AppbarWidget extends StatelessWidget implements PreferredSizeWidget {
   Widget build(BuildContext context) {
     return AppBar(
       title: Text(label, style: TextStyle(fontWeight: FontWeight.w600)),
+      actions: [
+        PopupMenuButton(
+          itemBuilder: (BuildContext context) {
+            return [
+              const PopupMenuItem(
+                value: 'New playlist',
+                child: Text('New playlist'),
+              ),
+
+              const PopupMenuItem(
+                value: 'New Album',
+                child: Text('New Album')
+              ),
+
+              const PopupMenuItem(
+                value: 'Share Playlist',
+                child: Text('Share Playlist')
+              ),
+
+              const PopupMenuItem(
+                value: 'Share Album',
+                child: Text('Share Album')
+              ),
+            ];
+          },
+        ),
+      ],
     );
   }
 
